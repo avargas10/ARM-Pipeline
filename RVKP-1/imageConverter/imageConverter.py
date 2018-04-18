f8e7963f1daf8b6019b0747b583a178260989324
avargas10 <afelipe.vargas.r@gmail.com>
2018-04-18 10:41:26 -0600

    image converter
    Python file to convert image file into pixels value from 0-255 into a txt file

--- RVKP-1/imageConverter/imageConverter.py
+++ RVKP-1/imageConverter/imageConverter.py
@@ -1,10 +1,59 @@
-from PIL import Image
+import PIL.Image
+from tkinter import * 
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from numpy import binary_repr
 def convert(image,txt):
-    foto = Image.open(image)
+    print("Converting " + image)
+    fp = open(image,"rb")
+    foto = PIL.Image.open(fp)
+    #foto = Image.open(image)
+    width, height = foto.size
+    if(width!=640 or height!=480):
+        print('Size needs to be 640x480')
+        return 0
     datos = list(foto.getdata())
     foto.close()
-    f = open (txt)
+    f = open (txt,'w')
+    
     for elemento in datos:
-        f.write('%s \n' % elemento)
+        binario = binary_repr(elemento[0], width=8)
+        f.write('{:03d}\n'.format(binario))
+        #print(elemento)
     f.close()
-convert("image.jpg","image.txt")
+    print ("Convertion Completed")
+    print ("Total of pixels: ")
+    print(len(datos))
+    
+#convert("image.jpg","image.txt")
+def fileExplorer():
+    #Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    return filename
+class GUI(Frame):
+    def __init__(self,master=None):
+        Frame.__init__(self, master)
+        self.grid()
+        self.filename = ''
+        self.fnameLabel = Label(master, text="Welcome to image pixel converter")
+        self.fnameLabel.grid()
+        self.lnameLabel = Label(master, text="Select an image")
+        self.lnameLabel.grid()
+        
+        def buttonClick():
+            self.filename = fileExplorer()
+            print("Image Ready to convert")
+
+        def convertClick():
+            if(self.filename!=''):
+                convert(self.filename,'image.txt')
+                
+        self.submitButton = Button(master, text="Search Image", command=buttonClick)
+        self.submitButton.grid()
+        self.submitButton = Button(master, text="Load Image", command=convertClick)
+        self.submitButton.grid()
+
+if __name__ == "__main__":
+    guiFrame = GUI()    
+    guiFrame.mainloop()
+
